41271a8b8c2b3dca8bb4b97d2c7d7279cf4ee403
Benjamin Pollak <benjaminwpollak@gmail.com>
2018-03-18 13:06:19 -0500

    renaming

--- static/src/App.js
+++ static/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header.js';
+import Navbar from './Navbar.js';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-        <Header/>
+        <Navbar/>
         
         <p>
           <center> Choose a file to analyze
---
+++ static/src/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+  render() {
+      return (
+        <div className="Nav">
+          <header className="App-header">
+            <h1>
+              <center> Sherlock </center>
+            </h1>
+          </header>
+        </div>
+      )
+  }
+}
+
+export default Navbar
