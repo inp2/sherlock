07b10621a9410dc8ba84f0e6f24617d329405c8b
Benjamin Pollak <benjaminwpollak@gmail.com>
2018-03-19 14:42:48 -0500

    navbar updates

--- static/src/Navbar.js
+++ static/src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Navbar.css';
+import logo from './edited_logo.png';
 
 class Navbar extends Component {
   render() {
@@ -7,7 +8,10 @@ class Navbar extends Component {
         <div className="Nav">
           <header className="App-header">
             <h1>
-              <center> Sherlock </center>
+              <center>
+                <img className="logo" src={logo} alt="logo" height={80} width={80} />
+                Sherlock 
+              </center>
             </h1>
           </header>
         </div>
