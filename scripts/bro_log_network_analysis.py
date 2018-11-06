2ae426cfb598b97fd9d7eaef038d26228bd1f92a
inp2 <ipalmer2@illinois.edu>
2018-11-05 18:47:35 -0600

    log networkx analysis

--- scripts/bro_log_network_analysis.py
+++ scripts/bro_log_network_analysis.py
@@ -30,23 +30,8 @@ DG = nx.DiGraph()
 name1 = ''
 name2 = ''
 for index, row in bro_df.iterrows():
-    try: 
-        name1 = socket.gethostbyaddr(str(row['id.orig_h']))
-    except:
-        G.add_node(row['id.orig_h'])
-    try:
-        name2 = socket.gethostbyaddr(str(row['id.resp_h']))
-    except:
-        G.add_node(row['id.resp_h'])
-    if name1 and name2:
-        G.add_edge(name1[0], name2[0])
-    elif name1:
-        G.add_edge(name1[0], row['id.resp_h'])
-    elif name2:
-        G.add_edge(row['id.orig_h'], name2[0])
+    G.add_edge(row[0], row[0])
 
-# pos = graphviz_layout(G, prog='dot')
-# nx.draw(G, pos, with_labels=True, node_size=100, node_color='w')
-# plt.savefig('directed_networkx_graph.png')
-write_dot(G, "test1.dot")
-write_dot(DG, "test2.dot")
+pos = graphviz_layout(G, prog='dot')
+nx.draw(G, pos, with_labels=False, node_size=100, node_color='w')
+plt.savefig('directed_networkx_graph.png')
